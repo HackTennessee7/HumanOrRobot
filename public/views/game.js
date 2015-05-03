2ae726d4f65a7fdf6862960c363e0e8107b8c080
YOUR NAME <erinpagemd@yahoo.com>
2015-05-02 22:20:23 -0500

    cycling through array of images when clicking next in modal

--- public/views/game.js
+++ public/views/game.js
@@ -1,7 +1,12 @@
 angular
 .module('hor')
-.controller('GameController',function($scope,arrayFactory){
+.controller('GameController',function($scope, arrayFactory){
   $scope.images = arrayFactory;
 
+  $scope.show = 0;
+
+  $scope.nextImage = function (){
+    $scope.show += 1;
+  }
 
 })
