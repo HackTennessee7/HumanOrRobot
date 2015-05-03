fb13b48598a7f56d6faa809ef7a6060c1680aa06
YOUR NAME <erinpagemd@yahoo.com>
2015-05-03 01:38:05 -0500

    deploying to gh-pages

---
+++ models/arrayfactory.js
@@ -0,0 +1,51 @@
+angular
+.module('hor')
+.factory('arrayFactory', function(){
+  var images = [
+    {
+      url: 'assets/img/human/Anastasia.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Arnold.jpg',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/C3P0.jpg',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Hara.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Hiroshi.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Justin.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Samantha.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Sarah.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Scientist.png',
+      hof: 'Human'
+    },
+    {
+      url: 'assets/img/human/Shpagina.jpg',
+      hof: 'Human'
+    }
+
+  ]
+
+  images = _.shuffle(images)
+  
+  return images
+});
